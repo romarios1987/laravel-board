3cc9272fa15fcc38fe995df9fe04de0c2ed251d6
romarios1987 <romios047@gmail.com>
2021-02-27 18:57:39 +0200

    added laravel-ide-helper & set up laravel-mix & first step auth for users

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+    public const CABINET = '/cabinet';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
